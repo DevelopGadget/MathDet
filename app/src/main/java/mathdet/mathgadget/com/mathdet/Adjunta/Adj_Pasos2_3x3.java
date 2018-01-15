4e3fb348cff9d7569821dc48f62d69c18da78219
FernanGamerFlo <33295357+FernanGamerFlo@users.noreply.github.com>
2018-01-15 10:07:41 -0500

    Adjunta 3x3 Vista Completa

---
+++ app/src/main/java/mathdet/mathgadget/com/mathdet/Adjunta/Adj_Pasos2_3x3.java
@@ -0,0 +1,16 @@
+package mathdet.mathgadget.com.mathdet.Adjunta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mathdet.mathgadget.com.mathdet.R;
+
+public class Adj_Pasos2_3x3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adj_pasos2_3x3);
+
+    }
+}
--- app/src/main/java/mathdet/mathgadget/com/mathdet/Adjunta/Adj_Pasos_3x3.java
+++ app/src/main/java/mathdet/mathgadget/com/mathdet/Adjunta/Adj_Pasos_3x3.java
@@ -1,15 +1,37 @@
 package mathdet.mathgadget.com.mathdet.Adjunta;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import mathdet.mathgadget.com.mathdet.R;
 
-public class Adj_Pasos_3x3 extends AppCompatActivity {
+public class Adj_Pasos_3x3 extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adj_pasos_3x3);
+        findViewById(R.id.Paso1).setOnClickListener(this);
+        findViewById(R.id.Paso2).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.Paso1:
+                startActivity(new Intent(getApplicationContext(), Adj_Pasos1_3x3.class));
+                break;
+            case R.id.Paso2:
+                startActivity(new Intent(getApplicationContext(), Adj_Pasos2_3x3.class));
+                break;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 }
