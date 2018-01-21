47a693465dfbda3f08695eaf43d50df2866863d2
FernanGamerFlo <33295357+FernanGamerFlo@users.noreply.github.com>
2018-01-20 19:17:15 -0500

    Adjunta Paso 4x4 vista completa

---
+++ app/src/main/java/mathdet/mathgadget/com/mathdet/Adj_Pasos_4x4.java
@@ -0,0 +1,13 @@
+package mathdet.mathgadget.com.mathdet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Adj_Pasos_4x4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adj_pasos_4x4);
+    }
+}
